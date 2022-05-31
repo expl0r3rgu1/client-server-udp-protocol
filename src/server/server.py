ca85cbd9299ca68c8b10379212866044217e12cd
expl0r3rgu1 <salvaddimando@gmail.com>
2022-05-31 18:41:12 +0200

    Created start_server method

--- src/server/server.py
+++ src/server/server.py
@@ -6,3 +6,17 @@ from _thread import *
 host = "127.0.0.1"
 port = 10000
 buffer_size = 1024
+
+def start_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind((host, port))
+
+    print("Server started and listening on port: " + str(port))
+
+    while True:
+        data, addr = server_socket.recvfrom(buffer_size)
+        print("Received message from: " + str(addr))
+        print("Message: " + str(data))
+        
+
+start_server()
