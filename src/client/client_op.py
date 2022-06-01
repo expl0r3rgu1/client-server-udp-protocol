62dee1e828a8209c3f8f64d42ee35d8b30847d7f
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-06-01 13:21:22 +0200

    Created list_files function

--- src/client/client_op.py
+++ src/client/client_op.py
@@ -3,3 +3,13 @@
 FILES_PATH = './files'
 EOF = b' /EOF/ \r\n/'
 BUFFER_SIZE = 1024
+
+def list_files(server_socket, addr):
+    server_socket.sendto(cmd.encode(), server_address)
+    data, addr = server_socket.recvfrom(buffer_size)
+    files_num = int(data.decode("utf-8"))
+
+    print("Received files number: " + str(files_num))
+    for file in range(files_num):
+        data, addr = server_socket.recvfrom(buffer_size)
+        print(data.decode("utf-8"))
