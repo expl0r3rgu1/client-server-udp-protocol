5c127bb1a60f218f17172951e6840470fdd22b00
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-06-01 13:35:18 +0200

    Fixed references to BUFFER_SIZE

--- src/client/client_op.py
+++ src/client/client_op.py
@@ -6,18 +6,18 @@ BUFFER_SIZE = 1024
 
 def list_files(server_socket, server_address, cmd):
     server_socket.sendto(cmd.encode(), server_address)
-    data, addr = server_socket.recvfrom(buffer_size)
+    data, addr = server_socket.recvfrom(BUFFER_SIZE)
     files_num = int(data.decode("utf-8"))
 
     print("Received files number: " + str(files_num))
     for file in range(files_num):
-        data, addr = server_socket.recvfrom(buffer_size)
+        data, addr = server_socket.recvfrom(BUFFER_SIZE)
         print(data.decode("utf-8"))
 
 def get_file(server_socket, server_address, cmd):
     server_socket.sendto(cmd.encode(), server_address)
     filename = cmd.split()[1]
-    data, addr = server_socket.recvfrom(buffer_size)
+    data, addr = server_socket.recvfrom(BUFFER_SIZE)
 
     if data.decode("utf-8") == "OK":
         print("Downloading file: " + filename)
@@ -25,7 +25,7 @@ def get_file(server_socket, server_address, cmd):
             os.mkdir(FILES_PATH)
         file = open(FILES_PATH + filename, 'wb')
         while True:
-            data, addr = server_socket.recvfrom(buffer_size)
+            data, addr = server_socket.recvfrom(BUFFER_SIZE)
             if data == EOF:
                 break
             file.write(data)
@@ -43,7 +43,7 @@ def upload_file(server_socket, server_address, cmd):
         server_socket.sendto(cmd.encode(), server_address)
         file = open(FILES_PATH + filename, 'rb')
         while True:
-            data = file.read(buffer_size)
+            data = file.read(BUFFER_SIZE)
             if not data:
                 server_socket.sendto(eof, server_address)
                 break
