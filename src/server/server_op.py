9656b894222ba6e698fc9a9e9e9747f83e74b0cd
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-06-01 12:42:46 +0200

    Created send_file function

--- src/server/server_op.py
+++ src/server/server_op.py
@@ -1,9 +1,25 @@
 # server operations
 FILES_PATH = './files'
 
-def list_files(server_socket, cmd, addr):
+def list_files(server_socket, addr):
     files = os.listdir(FILES_PATH)
     server_socket.sendto(str(len(files)).encode("utf-8"), addr)
     for file in files:
         server_socket.sendto(file.encode("utf-8"), addr)
     print("List of files sent")
+
+def send_file(server_socket, filename, addr):
+    if os.path.exists(FILES_PATH + '/' + filename):
+        print("Sending file: " + filename)
+        server_socket.sendto("OK".encode("utf-8"), addr)
+        file = open(FILES_PATH + '/' + filename, 'rb')
+        while True:
+            data = file.read(BUFFER_SIZE)
+            if not data:
+                server_socket.sendto(EOF, addr)
+                break
+            server_socket.sendto(data, addr)
+        file.close()
+        print("File " + filename + " sent")
+    else:
+        server_socket.sendto("NOT OK".encode("utf-8"), addr)
